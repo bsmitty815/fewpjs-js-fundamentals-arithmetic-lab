e0cb9b77ac50155a6a32d98613656b2a15566390
Brian Smith <brian.bubbam@gmail.com>
2021-05-17 15:26:02 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+var num3 = 34;
+var num4 = 10;
+var mod = num3 % num4;
+
+const max = Math.max( 2, 20 )
